330af730991124d63eb3ead95079f9d1ec0b7c40
ILYA AGEEV <agvilya@gmail.com>
2022-10-31 21:28:13 +0700

    5 steps is complete

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 import json
-from flask import Flask
+from flask import Flask,jsonify,request
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -10,11 +10,15 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
+users_json = 'users.json'
+orders_json = 'orders.json'
+offers_json = 'offers.json'
+
 
 class User(db.Model):
     __tablename__ = "users"
 
-    id = db.Column(db.Integer,primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(50))
     last_name = db.Column(db.String(50))
     age = db.Column(db.Integer)
@@ -22,69 +26,115 @@ class User(db.Model):
     role = db.Column(db.String(100))
     phone = db.Column(db.String(100))
 
-
-db.create_all()
+    def as_dict(self):
+        return {
+            "id": self.id,
+            "first_name": self.first_name,
+            "last_name": self.last_name,
+            "age": self.age,
+            "email": self.email,
+            "role": self.role,
+            "phone": self.phone,
+        }
 
 
 class Order(db.Model):
     __tablename__ = "orders"
 
-    id = db.Column(db.Integer,primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50))
     description = db.Column(db.String(50))
-    start_date = db.Column(db.Date)
-    end_date = db.Column(db.Date)
+    start_date = db.Column(db.String(50))
+    end_date = db.Column(db.String(50))
     address = db.Column(db.String(300))
     price = db.Column(db.Integer)
-    customer_id = db.Column(db.Integer,db.ForeignKey(f"{User.__tablename__}.id"))
-    executor_id = db.Column(db.Integer,db.ForeignKey(f"{User.__tablename__}.id"))
+    customer_id = db.Column(db.Integer, db.ForeignKey(f"{User.__tablename__}.id"))
+    executor_id = db.Column(db.Integer, db.ForeignKey(f"{User.__tablename__}.id"))
 
-
-db.create_all()
+    def as_dict(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "description": self.description,
+            "start_date": self.start_date,
+            "end_date": self.end_date,
+            "address": self.address,
+            "price": self.price,
+            "customer_id": self.customer_id,
+            "executor_id": self.executor_id,
+        }
 
 
 class Offer(db.Model):
     __tablename__ = "offers"
 
-    id = db.Column(db.Integer,primary_key=True)
-    order_id = db.Column(db.Integer,db.ForeignKey(f"{Order.__tablename__}.id"))
-    executor_id = db.Column(db.Integer,db.ForeignKey(f"{User.__tablename__}.id"))
+    id = db.Column(db.Integer, primary_key=True)
+    order_id = db.Column(db.Integer, db.ForeignKey(f"{Order.__tablename__}.id"))
+    executor_id = db.Column(db.Integer, db.ForeignKey(f"{User.__tablename__}.id"))
 
-
-db.create_all()
+    def as_dict(self):
+        return {
+            "id": self.id,
+            "order_id": self.order_id,
+            "executor_id": self.executor_id,
+        }
 
 
 def load_json(file_json):
     """Чтение данных из json-файла"""
-    with open(file_json,mode='r',encoding='utf-8') as file:
+    with open(file_json, mode='r', encoding='utf-8') as file:
         data = json.load(file)
         return data
 
 
-def users_db_load(db_table):
+def db_load(file_json, model):
     """Загрузка данных в БД"""
     with app.app_context():
-        users = load_json(os.path.join('data','users.json'))
-        for user in users:
-            db.session.add(
-                db_table(
-                    id=user.id,
-                    first_name=user.first_name,
-                    last_name=user.last_name,
-                    age=user.age,
-                    email=user.email,
-                    role=user.role,
-                    phone=user.phone
-                )
-            )
+        data = load_json(os.path.join('data', file_json))
+        for i in data:
+            db.session.add(model(**i))
         db.session.commit()
 
 
-@app.route('/users')
+@app.route('/users', methods=["GET", "PUT"])
 def users_page():
-    return User.query.all()
+    if request.method == "GET":
+        return jsonify([user.as_dict() for user in User.query.all()])
+    if request.method == "PUT":
+        pass
 
 
-with app.app_context():
-    users_db_load(User)
-    app.run(debug=True)
+@app.route('/users/<int:pk>')
+def user_page(pk):
+    return jsonify(User.query.get(pk).as_dict())
+
+
+@app.route('/orders', methods=["GET", "PUT"])
+def orders_page():
+    return jsonify([order.as_dict() for order in Order.query.all()])
+
+
+@app.route('/orders/<int:pk>')
+def order_page(pk):
+    return jsonify(Order.query.get(pk).as_dict())
+
+
+@app.route('/offers', methods=["GET", "PUT"])
+def offers_page():
+    return jsonify([offer.as_dict() for offer in Offer.query.all()])
+
+
+@app.route('/offers/<int:pk>')
+def offer_page(pk):
+    return jsonify(Offer.query.get(pk).as_dict())
+
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.drop_all()
+        db.create_all()
+        db_load(users_json, User)
+        db_load(offers_json, Offer)
+        db_load(orders_json, Order)
+
+        app.run(debug=True)
